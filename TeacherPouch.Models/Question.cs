48430b82f2d0fdbe4b3798825887a077d86dd6ec
Darren <darren@steinwegs.com>
2014-03-06 22:09:15 -0600

    Add Question model.

---
+++ TeacherPouch.Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeacherPouch.Models
+{
+    public class Question
+    {
+        public int ID { get; set; }
+        public int PhotoID { get; set; }
+        public string Text { get; set; }
+        public string SentenceStarters { get; set; }
+        public int? Order { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", this.ID, this.Text);
+        }
+    }
+}
